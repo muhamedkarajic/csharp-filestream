38da0c3cd9c764614d66d04f7ee4d475dd225a08
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2021-06-27 23:43:46 +0200

    Initial commit

---
+++ DataProcessor/FileProcessor.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+
+namespace DataProcessor
+{
+    class FileProcessor
+    {
+        public string InputFilePath { get; set; }
+
+
+        public FileProcessor(string filePath)
+        {
+            InputFilePath = filePath;
+        }
+
+        public void Process()
+        {
+            Console.WriteLine($"Begin process of {InputFilePath}");
+            if (!File.Exists(InputFilePath))
+            {
+                Console.WriteLine($"ERROR: File {InputFilePath} dose not exist.");
+                return;
+            }
+        }
+    }
+}
---
+++ DataProcessor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DataProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parsing command line options");
+
+            var command = args[0];
+
+            if(command == "--file")
+            {
+                var filePath = args[1];
+                Console.WriteLine($"Single file {filePath} selected");
+                ProcessingSingleFile(filePath);
+            }
+        }
+
+        private static void ProcessingSingleFile(string filePath)
+        {
+            var fileProcessor = new FileProcessor(filePath);
+            fileProcessor.Process();
+        }
+    }
+}
