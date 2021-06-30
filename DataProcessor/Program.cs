ffb73fd3de627c33bd0fd1765c448f36d9494c18
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2021-06-30 16:16:31 +0200

    Enumerating All the Files in a Directory

--- DataProcessor/Program.cs
+++ DataProcessor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace DataProcessor
 {
@@ -10,11 +11,22 @@ namespace DataProcessor
 
             var command = args[0];
 
-            if(command == "--file")
+            if (command == "--file")
             {
                 var filePath = args[1];
                 Console.WriteLine($"Single file {filePath} selected");
                 ProcessingSingleFile(filePath);
+            }
+            else if (command == "--dir")
+            {
+                var directoryPath = args[1];
+                var fileType = args[2];
+                Console.WriteLine($"Directory {directoryPath} selected for {fileType} files");
+                ProcessDirectory(directoryPath, fileType);
+            }
+            else
+            {
+                Console.WriteLine("Invalid command line options.");
             }
         }
 
@@ -22,6 +34,26 @@ namespace DataProcessor
         {
             var fileProcessor = new FileProcessor(filePath);
             fileProcessor.Process();
+        }
+
+        private static void ProcessDirectory(string directoryPath, string fileType)
+        {
+            var allFiles = Directory.GetFiles(directoryPath);
+
+            switch (fileType)
+            {
+                case "TEXT":
+                    string[] textFiles = Directory.GetFiles(directoryPath, "*.txt");
+                    foreach (var textFilePath in textFiles)
+                    {
+                        var fileProcessor = new FileProcessor(textFilePath);
+                        fileProcessor.Process();
+                    }
+                    break;
+                default:
+                    Console.WriteLine($"ERROR: {fileType} is not supported.");
+                    return;
+            }
         }
     }
 }
